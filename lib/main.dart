12f33128b845e689a9846205dbff5b42ab4db5e2
ersalanelang <ersa.kusuma.ek@gmail.com>
2022-02-27 14:34:26 +0700

    Tugas 1 - Layout FLuuter

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Login Screen"),
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 145, 0, 40),
+                  alignment: Alignment.center,
+                  child:  Image.asset("asset/image/flutter.png")),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  width: 330.0,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0)),
+                      hintText: 'Email'),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
+                  width: 330.0,
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40.0)),
+                      hintText: 'Password'),
+                    ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  width: 330.0,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blueAccent,
+                    ),
+                    child: Text('Login'),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    primary: Colors.black45,
+                  ),
+                  child: Text('Forgot Password?'),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(17, 45, 10, 0),
+                  width: 330.0,
+                  child: Text("Created by Ersalan Elang Kusuma - 123190163"),
+                )
+              ]
+          ),
+        ),
+      ),
+    );
+  }
+}
